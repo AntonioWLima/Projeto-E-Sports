c21e8cbd0ba6a0e3a491063f12d583e5267129e0
AntonioWLima <antoniowagnerlima2411@gmail.com>
2025-07-13 01:12:21 -0300

    Retirada do API Key

--- script.js
+++ script.js
@@ -10,7 +10,6 @@ const markdownToHTML = (text) => {
     return converter.makeHtml(text)
 }
 
-// AIzaSyDWgeotHFU3EmD5kMcgod3wqPZ0BeBjASE
 const perguntarAI = async (question, game, apiKey) => {
     const model = "gemini-2.5-flash"
     const geminiURL = `https://generativelanguage.googleapis.com/v1beta/models/${model}:generateContent?key=${apiKey}`
