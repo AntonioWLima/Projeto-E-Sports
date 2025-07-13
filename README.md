6a470d0d318c7025a622b5e8e2f5ea3da0cb2cdd
AntonioWLima <antoniowagnerlima2411@gmail.com>
2025-07-13 01:10:11 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,122 @@
+# HTML
+
+- HyperText
+    - Texto puros, livros, revistas, jornais
+    - Links: acessar outros documentos
+    - Imagens, vídeos, áudios
+- Markup
+    - Marcação
+    - Tags: <a>Link</a>
+    - Atributos: <a href="https://instagram.com.br">Instagram</a>
+    - globais: id, class, ...
+- Language
+    - Linguagem
+    - Sintaxe: manneira de escrever
+
+# HTTP
+
+- HyperText
+- Tranfer
+    - Tranferência
+- Protocol
+    - Protocolo
+    - Conjunto de regras
+-  Methods HTTP (verbos): Get, Post, Patch/Put, Delete, ...
+- Headers (cabeçalhos): Instruções/Informações extras para cada chamada
+
+# URL
+- Uniform
+    - Uniforme
+- Resource
+    - Recurso
+- Locator
+    - Localizador
+    - Encontrar um recurso (html, css, js, pdf, png, jpg, mp4, mp3, ...)
+    - Endereço
+
+# IP
+- Internet
+    - Rede mundial de computadores
+- Protocol
+    - Conjunto de regras
+- Endereço do computador
+    - instagram.com.br:80 (dominio)
+    - 123.32.1.23 (ip)
+
+# DNS
+- Domain
+    - Dominio
+- Name
+    - Nome
+- Server
+    - Servidor
+
+# CSS
+- Cascading
+    - Cascata
+    - Regras das escritas
+    - Hierarquia
+    - Especificidade
+- Style
+    - Estilo
+- Sheet
+    - Folha
+
+De um arquivo que terá na sua sintaxe, declarações, propriedades e valores.
+A partir disso, o HTML é impactado visualmente.
+
+# JS
+- Linguagem de programação
+- Browsers
+- Input -> Process -> Output
+- Variáveis
+- Function
+    - Agrupamento e reuso de código
+    - Sequencia lógica
+    - Saída
+- Gemini (IA): esperar uma resposta
+- Mexer no meu navegador
+- Estrutura de dados
+- Estrutura de decisão
+- Algoritmo: sequencia de passos lógica, de maneira ordenada, a fim de chegar a alguma conclusão (limitada)
+- Lógica (Pensamento computacional)
+    - Abstração
+    - Decomposição
+    - Reconhecimento de padrão
+    - Algoritmo
+- Tipos de dados
+    - Number (número)
+    - String (textos) (" ", ' ', ` `)
+    - Boolean: true | false
+    - Objects
+
+# DOM
+- Document
+- Object
+- Model
+
+# API
+- Aplication
+- Programming
+- Interface
+- JSON: JavaScript Object **Notation**
+
+# CDN
+- Content
+- Delivery
+- Network
+
+# LLM
+- Large
+- Languange
+- Model
+- Gemini, OpenAI (chatGPT), Athropic (Claude) ...
+
+# Agentes AI
+- Tools (ferramentas): permitem uso de código, ou apps ou qualquer informação extra.
+- Melhor contexto
+
+# Engenharia de prompt
+- One shot: uma unica pergunta sem muito contexto
+- Few shot: é apresentado exemplo do que se espera
+- Chain of thought: cadeia de pensamento para a IA responder gradativamente conforme a instrução
---
+++ script.js
@@ -0,0 +1,138 @@
+const apiKeyInput = document.getElementById('apiKey')
+const gameSelect = document.getElementById('gameSelect')
+const questionInput = document.getElementById('questionInput')
+const askButton = document.getElementById('askButton')
+const aiResponse = document.getElementById('aiResponse')
+const form = document.getElementById('form')
+
+const markdownToHTML = (text) => {
+    const converter = new showdown.Converter()
+    return converter.makeHtml(text)
+}
+
+// AIzaSyDWgeotHFU3EmD5kMcgod3wqPZ0BeBjASE
+const perguntarAI = async (question, game, apiKey) => {
+    const model = "gemini-2.5-flash"
+    const geminiURL = `https://generativelanguage.googleapis.com/v1beta/models/${model}:generateContent?key=${apiKey}`
+    const perguntaLOL = `
+        ## Especialidade
+        Você é um especialista assistente de meta para o jogo ${game}.
+
+        ## Tarefa
+        Você deve responder as perguntas do usuário com base no seu conhecimento do jogo, estratégias, builds e dicas.
+
+        ## Regras
+        - Se você não sabe a resposta, responda com 'Não sei' e não tente inventar uma resposta.
+        - Se a pergunta não está relacionada ao jogo, resposta com 'Essa pergunta não está relacionada ao jogo'.
+        - Considere a data atual ${new Date().toLocaleDateString()}.
+        - Faça pesquisas atualizadas sobre o patch atual, baseado na data atual para dar uma resposta coerente.
+        - Nunca responda itens que você não tenha certeza de que existe no patch atual.
+
+        ## Resposta
+        - Economize na resposta, seja direto e responda no máximo 500 caracteres.
+        - Responda em markdown.
+        - Não precisa fazer nenhuma saudação ou despedida, apenas responda o que o usuário está querendo.
+
+        ## Exemplo de resposta
+        Pergunta do usuário: Melhor build rengar jungle?
+        Resposta: A build mais atual é: \n\n **Itens:** Coloque os itens aqui.\n\n**Runas:** Exemplo de runas\n\n
+
+        ---
+        Aqui está a pergunta do usuário: ${question}
+    `
+
+    const perguntaEpic7 = `
+        ## Especialidade
+        Você é um especialista assistente de meta para o jogo ${game}.
+
+        ## Tarefa
+        Você deve responder as perguntas do usuário com base no seu conhecimento do jogo, estratégias, builds, historia do personagem e dicas.
+
+        ## Regras
+        - Se você não sabe a resposta, responda com 'Não sei' e não tente inventar uma resposta.
+        - Se a pergunta não está relacionada ao jogo, responda com 'Essa pergunta não está relacionada ao jogo'.
+        - Considere a data atual ${new Date().toLocaleDateString()}.
+        - Faça pesquisas atualizadas sobre o patch atual, baseado na data atual para dar uma resposta coerente.
+        - Nunca responda itens que você não tenha certeza de que existe no patch atual.
+
+        ## Resposta
+        - Economize na resposta, seja direto e responda no máximo 500 caracteres.
+        - Responda em markdown.
+        - Não precisa fazer nenhuma saudação ou despedida, apenas responda o que o usuário está querendo.
+
+        ## Exemplo de resposta
+        Pergunta do usuário: Qual é a build para o personagem Ras?
+        Resposta: As builds mais usadas são: \n\n**Tipo de conjunto**\n\n coloque as variações de conjuntos aqui.\n\n**Atributos principais: \n\nValor para cada atributo.\n\n**Artefato:** Nomes dos artefatos mais usados\n\n 
+
+        ---
+        Aqui está a pergunta do usuário: ${question}
+    `
+
+    let pergunta = ''
+
+    if (game == 'lol') {
+        pergunta = perguntaLOL
+    } else if (game == 'epic seven') {
+        pergunta = perguntaEpic7
+    }
+
+    console.log(pergunta)
+
+    const contents = [{
+        role: "user",
+        parts: [{
+            text: pergunta
+        }]
+    }]
+
+    const tools = [{
+        google_search: {}
+    }]
+
+    // chamada API
+    const response = await fetch(geminiURL, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            contents,
+            tools
+        })
+    })
+
+    const data = await response.json()
+    console.log({ data })
+    return data.candidates[0].content.parts[0].text
+}
+
+const enviarFormulario = async (event) => {
+    event.preventDefault()
+    const apiKey = apiKeyInput.value
+    const game = gameSelect.value
+    const question = questionInput.value
+
+    if (apiKey == '' || game == '' || question == '') {
+        alert('Por favor, preencha todos os campos')
+        return
+    }
+
+    askButton.disabled = true
+    askButton.textContent = 'Perguntando...'
+    askButton.classList.add('loading')
+
+    try {
+        // perguntar para a IA
+        const text = await perguntarAI(question, game, apiKey)
+        aiResponse.querySelector('.response-content').innerHTML = markdownToHTML(text)
+        aiResponse.classList.remove('hidden')
+
+    } catch (error) {
+        console.log('Erro: ', error)
+    } finally {
+        askButton.disabled = false
+        askButton.textContent = "Perguntar"
+        askButton.classList.remove('loading')
+    }
+}
+form.addEventListener('submit', enviarFormulario)
